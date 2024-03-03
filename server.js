4eda737800b2810eca51988d7c4d54fc8961e13c
unknown <ratnajeetpatil76@gmail.com>
2024-03-03 23:40:03 +0530

    added table

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const PDFDocument = require('pdfkit');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const { createWriteStream, createReadStream, unlink } = require('fs');
+const { resolve } = require('path');
+const { PassThrough } = require('stream');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -74,6 +78,24 @@ app.post("/attendance-data", async (req, res) => {
       return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+// Add a new endpoint to retrieve all attendance data
+app.get("/all-attendance-data", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM attendance");
+    
+    if (rows.length > 0) {
+      // Send the data as a JSON response
+      return res.status(200).json({ attendanceData: rows });
+    } else {
+      return res.status(404).json({ message: "No attendance data available" });
+    }
+  } catch (error) {
+    console.error('Error retrieving all attendance data:', error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
